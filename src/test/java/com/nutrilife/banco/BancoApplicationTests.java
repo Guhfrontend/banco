1120fac9e3c09af365a5a85a36b5c4c1c90824f1
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-09-20 22:35:48 -0300

    First Commit: Criação do projeto

---
+++ src/test/java/com/nutrilife/banco/BancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrilife.banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
